641c1a69b46d08bbcc1bea45cb1200163b0cb2ed
Maame Konadu Assasie-Gymah <maame@Maames-MacBook-Pro.local>
2021-06-20 15:34:06 -0400

    initial files

---
+++ index.js
@@ -0,0 +1,19 @@
+// TODO: Include packages needed for this application
+// require inquirer
+// require generatemarkdown package
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+// ask questions
+// call generatemarkdown function which will return a string
+// call writeto file function; ppass 
+// ppas
+
+// Function call to initialize app
+init();
